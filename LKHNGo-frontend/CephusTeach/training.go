4baccc2e08cfefaaead53bbc24f9ff56ff00bae7
christinacephus@pursuit.org <christinacephus@pursuit.org>
2024-01-21 00:14:16 -0500

    practicing runtime

--- LKHNGo-frontend/CephusTeach/training.go
+++ LKHNGo-frontend/CephusTeach/training.go
@@ -1,31 +1,44 @@
 // ----------------------------------------------------------------UDEMY CODING GoesToEleven
-
 package main
 
 import (
 	"fmt"
-	"sync"
-	"sync/atomic"
+	"runtime"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	var incrementor int64
-	gs := 100
-	wg.Add(gs)
-
-	for i := 0; i < gs; i++ {
-		go func() {
-			atomic.AddInt64(&incrementor, 1)
-			fmt.Println(incrementor)
-			wg.Done()
-		}()
-
-	}
-	wg.Wait()
-	fmt.Println("end value:", incrementor)
-
+	fmt.Println("Show me my OS & Architecture:",runtime.GOOS, runtime.GOARCH)
 }
+
+
+// ----------------------------------------------------------------UDEMY CODING GoesToEleven
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"sync/atomic"
+// )
+
+// func main() {
+// 	var wg sync.WaitGroup
+// 	var incrementor int64
+// 	gs := 100
+// 	wg.Add(gs)
+
+// 	for i := 0; i < gs; i++ {
+// 		go func() {
+// 			atomic.AddInt64(&incrementor, 1)
+// 			fmt.Println(incrementor)
+// 			wg.Done()
+// 		}()
+
+// 	}
+// 	wg.Wait()
+// 	fmt.Println("end value:", incrementor)
+
+// }
 
 // ----------------------------------------------------------------UDEMY CODING GoesToEleven
 // package main
