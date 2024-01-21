d94966b0f27306640c8ad34cea579adbbb59481a
christinacephus@pursuit.org <christinacephus@pursuit.org>
2024-01-20 20:53:03 -0500

    race conditions

--- LKHNGo-frontend/CephusTeach/training.go
+++ LKHNGo-frontend/CephusTeach/training.go
@@ -4,51 +4,91 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+	"time"
 )
 
-var wg sync.WaitGroup
-
 func main() {
-	var name string = "Cephus"
-	fmt.Printf("Hello World I'm, %s\t\n", name)
+	fmt.Println("how many CPU's running?:\t", runtime.NumCPU())
+	fmt.Println("how many go Routines running?:\t", runtime.NumGoroutine())
 
-	fmt.Println("OS\t", runtime.GOOS)
-	fmt.Println("Architecture\t", runtime.GOARCH)
-	fmt.Println("CPU's running\t", runtime.NumCPU())
-	fmt.Println("Live  Go Routines", runtime.NumGoroutine())
+	counter := 0
 
-	energy := []int{45, 42, 41, 40}
-	for _, v := range energy {
-		fmt.Printf("checking Energy Levels -- @%v\n", v)
-		fmt.Printf("the Type is -- %T\n", v)
+	const gz = 10
+	var wait sync.WaitGroup
+	wait.Add(gz)
+
+	for i := 0; i < gz; i++ {
+		go func() {
+			v := counter
+			//if you want to wait or have a timeout -- import time and give it some rest
+			time.Sleep(time.Second)
+			runtime.Gosched()
+			v++
+			counter = v
+			//now set your wait group to done
+			wait.Done()
+		}()
+		fmt.Println("how many go Routines running?:\t", runtime.NumGoroutine())
 
 	}
-	isBool, energyLevel := activateGate(39)
-	fmt.Println(isBool, energyLevel)
-
-	wg.Add(1)
-	go activateGate(40)
-	go activateGate(41)
-	go activateGate(42)
-	go activateGate(43)
-	wg.Wait()
-
+	//create the race condition below.
+	wait.Wait()
+	fmt.Println("how many go Routines running?:\t", runtime.NumGoroutine())
+	fmt.Println("count:\t", counter)
 
 }
 
-var energyLevel int = 5
-var energyPointer *int = &energyLevel
+// ----------------------------------------------------------------PAK CODING 1:1
+// package main
 
-var quantumCodes [5]int = [5]int{11, 33, 44, 5, 66}
+// import (
+// 	"fmt"
+// 	"runtime"
+// 	"sync"
+// )
 
-func activateGate(energyLevel int) (bool, int) {
-	if energyLevel > 40 {
-		fmt.Println("Energylevels are a go")
-		return true, energyLevel
-	} else {
-		fmt.Println("Energylevels are not a go!")
-		return false, energyLevel
-	}
-wg.Wait()
-return false, energyLevel
-}
+// var wg sync.WaitGroup
+
+// // func main() {
+// 	var name string = "Cephus"
+// 	fmt.Printf("Hello World I'm, %s\t\n", name)
+
+// 	fmt.Println("OS\t", runtime.GOOS)
+// 	fmt.Println("Architecture\t", runtime.GOARCH)
+// 	fmt.Println("CPU's running\t", runtime.NumCPU())
+// 	fmt.Println("Live  Go Routines", runtime.NumGoroutine())
+
+// 	energy := []int{45, 42, 41, 40}
+// 	for _, v := range energy {
+// 		fmt.Printf("checking Energy Levels -- @%v\n", v)
+// 		fmt.Printf("the Type is -- %T\n", v)
+
+// 	}
+// 	isBool, energyLevel := activateGate(39)
+// 	fmt.Println(isBool, energyLevel)
+
+// 	wg.Add(1)
+// 	go activateGate(40)
+// 	go activateGate(41)
+// 	go activateGate(42)
+// 	go activateGate(43)
+// 	wg.Wait()
+
+// }
+
+// var energyLevel int = 5
+// var energyPointer *int = &energyLevel
+
+// var quantumCodes [5]int = [5]int{11, 33, 44, 5, 66}
+
+// func activateGate(energyLevel int) (bool, int) {
+// 	if energyLevel > 40 {
+// 		fmt.Println("Energylevels are a go")
+// 		return true, energyLevel
+// 	} else {
+// 		fmt.Println("Energylevels are not a go!")
+// 		return false, energyLevel
+// 	}
+// // wg.Wait()
+// // return false, energyLevel
+// }
